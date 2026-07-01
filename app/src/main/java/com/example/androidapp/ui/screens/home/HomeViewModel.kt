c4ce125cc4418a283ab418bf13da9a5e54559d3e
johnmice05 <johnmice05@gmail.com>
2026-07-01 14:34:38 +0500

    Add HomeViewModel with MVVM pattern

---
+++ app/src/main/java/com/example/androidapp/ui/screens/home/HomeViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.androidapp.ui.screens.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+    // Add your ViewModel logic here
+}
