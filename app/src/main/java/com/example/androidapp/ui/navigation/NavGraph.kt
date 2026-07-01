90d3ec7efbfc5eff66bf57c68b9062e765feca5f
johnmice05 <johnmice05@gmail.com>
2026-07-01 14:35:21 +0500

    Add Navigation Graph setup

---
+++ app/src/main/java/com/example/androidapp/ui/navigation/NavGraph.kt
@@ -0,0 +1,19 @@
+package com.example.androidapp.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.androidapp.ui.screens.home.HomeScreen
+
+@Composable
+fun AppNavigation(navController: NavHostController? = null) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen()
+        }
+    }
+}
